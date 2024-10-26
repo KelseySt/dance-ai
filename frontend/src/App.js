3f8365f885058ce69ede25fec8c69fafab2e2001
Extorial <christopherwang197@gmail.com>
2024-10-25 22:56:08 -0400

    Install and Setup Tailwind, Set basic layout
    Install Tailwind for CSS frontend, setup config fields. Add basic layout and text to define where things where go.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,27 @@
-import logo from './logo.svg';
+
 import './App.css';
 import React, { useEffect, useState } from 'react';
+
+function VideoComponent() {
+  return (
+    <div className='grid grid-cols-2 justify-items-center w-full row-span-3 border border-black'>  
+      <div className='w-full justify-items-center mx-3 my-10 border border-black'>
+        Student Video
+      </div>
+      <div className='w-full justify-items-center mx-3 my-10 border border-black'>
+        Teacher Video
+      </div>
+    </div>
+  );
+}
+
+function BottomComponent() {
+  return (
+    <div className='w-full row-span-1 border border-black'>  
+      <h2>Bottom</h2>
+    </div>
+  );
+}
 
 function App() {
   const [data, setData] = useState(null);
@@ -13,8 +34,12 @@ function App() {
 
   return (
     <div className="App">
-      <h1>React + Flask Integration</h1>
-      {data ? <p>{data.message}</p> : <p>Loading...</p>}
+      {/* <h1>React + Flask Integration</h1>
+      {data ? <p>{data.message}</p> : <p>Loading...</p>}*/}
+      <div className='m-20 h-screen w-auto grid grid-rows-4 justify-items-center'>
+         <VideoComponent />
+         <BottomComponent />
+      </div>  
     </div>
   );
 }
