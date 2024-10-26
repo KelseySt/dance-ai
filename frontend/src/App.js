4d411c163675ad864a9a51962848eea63bbf1511
Extorial <christopherwang197@gmail.com>
2024-10-26 10:18:10 -0400

    Add Infobox component
    Add infobx component, popping up whenever a segment is clicked.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,7 +4,7 @@ import React, { useEffect, useState } from 'react';
 
 function TopComponent() {
   return (
-    <div className='grid grid-cols-2 justify-items-center w-full row-span-3 border border-black'>  
+    <div className='grid grid-cols-2 justify-items-center w-full row-span-2 border border-black'>  
       <VideoComponent title="Student"/>
       <VideoComponent title="Teacher"/>
     </div>
@@ -21,19 +21,32 @@ function VideoComponent( { title } ) {
 
 function BottomComponent() {
   return (
-    <div className='flex items-center grid grid-row-3 w-full row-span-1 border border-black'>  
-      <ProgressBarComponent pvalue=".30"/>
-      <div className="contents-center row-span-2"></div>
+    <div className='flex items-center grid grid-row-3 w-full row-span-2 border border-black'>  
+      <ProgressBarComponent progressValue="60"/> {/* progressValuee determines progress bar percentage */}
+      <div className="contents-center row-span-2">
+      </div>
+
     </div>
   );
 }
 
-function ProgressBarComponent( { pvalue } ) {
+function ProgressBarComponent( { progressValue } ) {
+  const [showInfobox, setShowInfobox] = useState(false); {/* Determines whether infobox appears */}
+  const [infoboxPosition, setInfoboxPosition] = useState(null);
+
+  function handleClick(position) {
+    setShowInfobox(!showInfobox);
+    setInfoboxPosition(position);
+  }
+
   return (
-    <div className="flex justify-center row-span-1 border border-green">
-      <div class="relative w-4/5 bg-gray-200 rounded-full h-2.5 dark:bg-gray-400">
-        <div class="bg-red-600 h-2.5 rounded-full w-[60%]"></div>
-        <PairOfBoxes firstPosition={1} secondPosition={3}/>
+    <div className="flex justify-center row-span-1">
+      <div className="relative w-4/5 bg-gray-200 rounded-full h-2.5 dark:bg-gray-400">
+        <div className="bg-red-600 h-2.5 rounded-full" style={{ width: `${progressValue}%`}}></div>
+        <PairOfBoxes firstPosition={55} secondPosition={60} onClick={() => {handleClick(57.5)}}/>
+        <PairOfBoxes firstPosition={20} secondPosition={23} onClick={() => {handleClick(21.5)}}/>
+
+        {showInfobox ? <InfoboxComponent position={infoboxPosition}/> : null}
       </div>
     </div>
     
@@ -42,28 +55,45 @@ function ProgressBarComponent( { pvalue } ) {
 
 function BlackBar( { progress }) {
   return (
-    <div className={"absolute top-0 border border-black bg-black w-0.5 h-2.5"}  style={{ left: `${progress}%` }}>
+    <div className={"z-10 absolute top-0 border border-black bg-black w-0.5 h-2.5"}  style={{ left: `${progress}%` }}>
     </div>
   );
 }
 
-function ClickableSegment({position, distance}) {
+function ClickableSegment({position, distance, onClick}) {
   return (
-    <div className='absolute top-0 h-2.5 cursor-pointer' style={{ left: `${position}%`, width: `${distance}%` }}>
+    <div 
+      className='absolute top-0 h-2.5 cursor-pointer bg-red-400' style={{ left: `${position}%`, width: `${distance}%` }} 
+      onClick={onClick}
+    >
     </div>
   );
 }
 
-function PairOfBoxes({firstPosition, secondPosition}) {
+function PairOfBoxes({firstPosition, secondPosition, onClick}) {
   return (
     <>
       <BlackBar progress={firstPosition} />
-      <ClickableSegment position={firstPosition} distance={(secondPosition-firstPosition)} />
+      <ClickableSegment position={firstPosition} distance={(secondPosition-firstPosition)} onClick={onClick}/>
       <BlackBar progress={secondPosition} />
     </>
   )
 }
 
+function InfoboxComponent({position}) {
+  return (
+    <div> 
+      <div
+        className="absolute bg-white border border-black p-2 rounded"
+        style={{ top: '30px', left: `${position}%`, transform: 'translateX(-50%)' }}
+      >
+        <h1>Timestamp</h1>
+        <h1>Feedback</h1>
+        <h1>Improvement</h1>
+      </div>
+    </div>
+  );
+}
 
 function App() {
   const [data, setData] = useState(null);
@@ -78,7 +108,7 @@ function App() {
     <div className="App">
       {/* <h1>React + Flask Integration</h1>
       {data ? <p>{data.message}</p> : <p>Loading...</p>}*/}
-      <div className='m-20 h-screen w-auto grid grid-rows-4 justify-items-center'>
+      <div className='m-10 h-screen w-auto grid grid-rows-4 justify-items-center'>
          <TopComponent />
          <BottomComponent />
       </div>  
