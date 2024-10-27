5333577a307babe2fdb9e6a7d9342873002861ca
Aanand S. <aanandsuryaa02@gmail.com>
2024-10-27 01:03:25 -0400

    Update Upload.js
    Added more blobs to add texture to the edges

--- frontend/src/Upload.js
+++ frontend/src/Upload.js
@@ -59,9 +59,13 @@ function Upload() {
 
     return (
         <div className="bg-orange-200 flex items-center justify-center h-screen w-screen">
-            <div className="absolute w-96 h-96 scale-125 bg-purple-500 rounded-full mix-blend-multiply filter blur-xl opacity-70 animate-blob animation-delay-2000"></div>
-            <div className="absolute w-96 h-96 scale-125 bg-blue-400 rounded-full mix-blend-multiply filter blur-xl opacity-50 animate-blob"></div>
-            <div className="absolute w-96 h-96 scale-125 bg-pink-500 rounded-full mix-blend-multiply filter blur-xl opacity-50 animate-blob animation-delay-4000"></div>
+            <div className="absolute inset-x-96 w-96 h-96 scale-125 bg-purple-500 rounded-full mix-blend-multiply filter blur-xl opacity-70 animate-blob animation-delay-2000"></div>
+            <div className="absolute inset-y-40 w-96 h-96 scale-125 bg-blue-400 rounded-full mix-blend-multiply filter blur-xl opacity-50 animate-blob"></div>
+            <div className="absolute right-96 bottom-10 inset-x--10 w-96 h-96 scale-125 bg-pink-500 rounded-full mix-blend-multiply filter blur-xl opacity-50 animate-blob animation-delay-4000"></div>
+            <div className="absolute left-0 top-0 w-60 h-60 bg-lime-500 rounded-full mix-blend-multiply filter blur-xl opacity-70 animate-blob animation-delay-2000"></div>
+            <div className="absolute right-11 bottom-12 w-44 h-44 scale-125 bg-blue-400 rounded-full mix-blend-multiply filter blur-xl opacity-50 animate-blob"></div>
+
+
             <div class="absolute bg-transparent scale-125 p-3 rounded-[30px] w-[350px] opacity-100 text-center shadow-2xl">
                 <h2 className="text-[2em] mb-[15px] text-white">Upload Your Videos</h2>
                 <form id="upload-form">
