5d4b48c1003fe77f52c3a7c33775805f95de5bb8
Jojo <joeljeny@gmail.com>
2024-10-27 01:57:41 -0400

    Navigation and Backend

--- frontend/src/Upload.js
+++ frontend/src/Upload.js
@@ -54,10 +54,47 @@ function Upload(props) {
             }
         };
     return (
-        <div className="bg-orange-200 flex items-center justify-center h-screen w-screen">
-            <div className="absolute w-96 h-96 scale-125 bg-purple-500 rounded-full mix-blend-multiply filter blur-xl opacity-70 animate-blob animation-delay-2000"></div>
-            <div className="absolute w-96 h-96 scale-125 bg-blue-400 rounded-full mix-blend-multiply filter blur-xl opacity-50 animate-blob"></div>
-            <div className="absolute w-96 h-96 scale-125 bg-pink-500 rounded-full mix-blend-multiply filter blur-xl opacity-50 animate-blob animation-delay-4000"></div>
+     <div className="bg-orange-200 flex items-center justify-center h-screen w-screen">
+            <div className="absolute inset-x-96 w-96 h-96 scale-125 bg-purple-500 rounded-full mix-blend-multiply filter blur-xl opacity-70 animate-blob animation-delay-2000"></div>
+            <div className="absolute inset-y-40 w-96 h-96 scale-125 bg-blue-400 rounded-full mix-blend-multiply filter blur-xl opacity-50 animate-blob"></div>
+            <div className="absolute right-96 bottom-10 inset-x--10 w-96 h-96 scale-125 bg-pink-500 rounded-full mix-blend-multiply filter blur-xl opacity-50 animate-blob animation-delay-4000"></div>
+            <div className="absolute left-0 top-0 w-60 h-60 bg-lime-500 rounded-full mix-blend-multiply filter blur-xl opacity-70 animate-blob animation-delay-2000"></div>
+            <div className="absolute right-11 bottom-12 w-44 h-44 scale-125 bg-blue-400 rounded-full mix-blend-multiply filter blur-xl opacity-50 animate-blob"></div>
+
+
+            <div class="absolute bg-transparent scale-125 p-3 rounded-[30px] w-[350px] opacity-100 text-center shadow-2xl">
+                <h2 className="text-[2em] mb-[15px] text-white">Upload Your Videos</h2>
+                <form id="upload-form">
+                <label for="refVideo" class="block bg-rose-200 border-2 border-white rounded-[15px] my-2 p-4 text-black cursor-pointer transition-colors duration-300 hover:bg-gray-300 shadow-md">
+                {refVideo}
+                <input type="file" id="refVideo" name="refVideo" accept="video/*" required className="hidden"
+                        onChange={(e) => handleFileChange(e, setrefVideo, setrefVideoFile)}
+                        handleSubmit/>
+                        </label>
+                <label for="userVideo" class="block bg-rose-200 border-2 border-white rounded-[15px] my-2 p-4 text-black cursor-pointer transition-colors duration-300 hover:bg-gray-300 shadow-md">
+                {userVideo}                                  
+                <input type="file" id="userVideo" name="userVideo" accept="video/*" required className="hidden"
+                        onChange={(e) => handleFileChange(e, setuserVideo, setuserVideoFile)}
+                        handleSubmit/>
+                        </label>
+                    <button onClick={handleSubmit} type="submit" class="block bg-green-500 text-white py-2 px-4 rounded-lg w-full cursor-pointer text-lg transition-colors duration-300 hover:bg-green-600">Upload Videos</button>
+                </form>
+                </div>
+                </div>   
+    );
+   }
+     export default Upload;
+
+
+/*
+<div className="bg-orange-200 flex items-center justify-center h-screen w-screen">
+            <div className="absolute inset-x-96 w-96 h-96 scale-125 bg-purple-500 rounded-full mix-blend-multiply filter blur-xl opacity-70 animate-blob animation-delay-2000"></div>
+            <div className="absolute inset-y-40 w-96 h-96 scale-125 bg-blue-400 rounded-full mix-blend-multiply filter blur-xl opacity-50 animate-blob"></div>
+            <div className="absolute right-96 bottom-10 inset-x--10 w-96 h-96 scale-125 bg-pink-500 rounded-full mix-blend-multiply filter blur-xl opacity-50 animate-blob animation-delay-4000"></div>
+            <div className="absolute left-0 top-0 w-60 h-60 bg-lime-500 rounded-full mix-blend-multiply filter blur-xl opacity-70 animate-blob animation-delay-2000"></div>
+            <div className="absolute right-11 bottom-12 w-44 h-44 scale-125 bg-blue-400 rounded-full mix-blend-multiply filter blur-xl opacity-50 animate-blob"></div>
+
+
             <div class="absolute bg-transparent scale-125 p-3 rounded-[30px] w-[350px] opacity-100 text-center shadow-2xl">
                 <h2 className="text-[2em] mb-[15px] text-white">Upload Your Videos</h2>
                 <form id="upload-form">
@@ -75,8 +112,6 @@ function Upload(props) {
                         </label>
                     <button type="submit" class="block bg-green-500 text-white py-2 px-4 rounded-lg w-full cursor-pointer text-lg transition-colors duration-300 hover:bg-green-600">Upload Videos</button>
                 </form>
-        </div>
-        </div>
-        );
-   }
-     export default Upload;
+                </div>
+                </div>
+*/
