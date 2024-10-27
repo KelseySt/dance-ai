4b7af1a290411660102b6a7c8b969c13a813ff4e
Extorial <christopherwang197@gmail.com>
2024-10-26 22:45:38 -0400

    Revert "React Router Added"
    This reverts commit 0a3a6a85ca59035e304a9a66558af2ff43b1b99d.

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,15 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './App.css';
 import App from './App';
-import Upload from './Upload';
-import { BrowserRouter as Router, Route, Routes, BrowserRouter } from 'react-router-dom';
+//import Upload from './Upload';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <BrowserRouter>
-      <Upload />
-  </BrowserRouter>
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
